4bd07f201acc9f42418522aed8d227d819ff5686
jeongye01 <74299317+jeongye01@users.noreply.github.com>
2022-03-16 22:50:22 +0900

    marker info

--- find-my/components/Marker.tsx
+++ find-my/components/Marker.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Map, MapMarker, ZoomControl, MapTypeControl, MapProps } from 'react-kakao-maps-sdk';
 
 interface Props {
@@ -13,6 +13,7 @@ interface Props {
 }
 
 function Marker({ position }: Props) {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <MapMarker
       position={{ lat: position.latlng.lat, lng: position.latlng.lng }}
@@ -29,10 +30,26 @@ function Marker({ position }: Props) {
           }, // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
         },
       }}
+      onClick={() => setIsOpen(true)}
     >
-      <div style={{ opacity: 0 }}>
-        <div className="rounded text-center w-40 h-14 bg-red-100">{position.title}</div>
-      </div>
+      {isOpen && (
+        <div style={{ minWidth: '150px' }}>
+          <img
+            alt="close"
+            width="14"
+            height="13"
+            src="https://t1.daumcdn.net/localimg/localimages/07/mapjsapi/2x/bt_close.gif"
+            style={{
+              position: 'absolute',
+              right: '5px',
+              top: '5px',
+              cursor: 'pointer',
+            }}
+            onClick={() => setIsOpen(false)}
+          />
+          <div style={{ padding: '5px', color: '#000' }}>{position.title}</div>
+        </div>
+      )}
     </MapMarker>
   );
 }
