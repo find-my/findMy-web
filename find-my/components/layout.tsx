3a4c4b4596a3ddd19f75d6e39b31292b8f1119bd
jeongye01 <74299317+jeongye01@users.noreply.github.com>
2022-03-20 03:34:26 +0900

    layout

--- find-my/components/layout.tsx
+++ find-my/components/layout.tsx
@@ -1,7 +1,16 @@
 interface Props {
+  title: string;
+  canGoBack: boolean;
+  hasTab: boolean;
   children: React.ReactNode;
 }
 
-export default function Layout({ children }: Props) {
-  return <div>{children}</div>;
+export default function Layout({ title, canGoBack, hasTab, children }: Props) {
+  return (
+    <div>
+      <div className="bg-white w-full"></div>
+      {children}
+      {hasTab ? <nav></nav> : null}
+    </div>
+  );
 }
--- find-my/pages/_app.tsx
+++ find-my/pages/_app.tsx
@@ -14,7 +14,9 @@ function MyApp({ Component, pageProps }: AppProps) {
         src="//dapi.kakao.com/v2/maps/sdk.js?appkey=7c979a8b28e8fbc552afe34a35bebecd&libraries=services,clusterer&autoload=false"
         strategy="beforeInteractive"
       />
-      <Component {...pageProps} />
+      <div className="w-full max-w-lg mx-auto">
+        <Component {...pageProps} />
+      </div>
     </>
   );
 }
