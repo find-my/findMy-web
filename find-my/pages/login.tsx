eb78d3a922921cc97480e5b38add9ecef606331f
jeongye01 <74299317+jeongye01@users.noreply.github.com>
2022-03-21 22:22:48 +0900

    hook form

--- find-my/pages/login.tsx
+++ find-my/pages/login.tsx
@@ -1,33 +1,54 @@
-import Image from 'next/image';
 import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faComment, faLock, faN } from '@fortawesome/free-solid-svg-icons';
+import { FieldErrors, useForm } from 'react-hook-form';
 import type { NextPage } from 'next';
+interface LoginForm {
+  email: string;
+  password: string;
+  errors?: string;
+}
 const Login: NextPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+    setError,
+  } = useForm<LoginForm>();
+  const onValid = (data: LoginForm) => {
+    console.log(data);
+  };
+  const onInvalid = (errors: FieldErrors) => {
+    console.log(errors);
+  };
   return (
     <main className="mt-16 w-full">
       <h3 className="text-5xl font-nanum-pen-script text-blue-400 text-center">어딨지?</h3>
       <div className="mt-12">
         <div className="flex flex-col items-center ">
           <h5 className="text-lg font-bold mb-3">로그인</h5>
-          <form className="flex flex-col w-full  px-20 pt-6 pb-8 mb-4">
+          <form onSubmit={handleSubmit(onValid, onInvalid)} className="flex flex-col w-full  px-20 pt-6 pb-8 mb-4">
             <div>
+              {errors.email?.message}
               <div className="mb-3 relative">
                 <div className="mr-1.5 text-2xl absolute left-2 top-1 text-gray-400">
                   <FontAwesomeIcon icon={faUser} />
                 </div>
                 <input
+                  {...register('email', { required: '이메일을 입력해 주세요.' })}
                   className="bg-white shadow appearance-none border  rounded w-full px-10 py-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-400"
-                  name="email"
                   type="email"
                   placeholder="이메일"
                 />
               </div>
+              {errors.password?.message}
               <div className="mb-3 relative">
                 <div className="mr-1.5 text-2xl absolute left-2 top-1 text-gray-400">
                   <FontAwesomeIcon icon={faLock} />
                 </div>
                 <input
+                  {...register('password', { required: '비밀번호를 입력해 주세요.' })}
                   className="bg-white shadow appearance-none border  rounded w-full px-10 py-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-400"
                   name="password"
                   type="password"
