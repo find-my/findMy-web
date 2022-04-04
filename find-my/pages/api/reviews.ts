00573cf13770adf65145f1bc81aed5f603f37895
jeongye01 <74299317+jeongye01@users.noreply.github.com>
2022-04-04 10:14:16 +0900

    Create reviews.ts
    /api/reviews 사용자가 받은 리뷰를 get

---
+++ find-my/pages/api/reviews.ts
@@ -0,0 +1,27 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import withHandler, { ResponseType } from '@libs/back/protectedHandler';
+import client from '@libs/back/client';
+import { withApiSession } from '@libs/back/session';
+
+async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
+  const {
+    session: { user },
+  } = req;
+  const reviews = await client.review.findMany({
+    where: {
+      createdForId: user?.id,
+    },
+    include: { createdBy: { select: { id: true, name: true, avatar: true } } },
+  });
+  res.json({
+    ok: true,
+    reviews,
+  });
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ['GET'],
+    handler,
+  }),
+);
