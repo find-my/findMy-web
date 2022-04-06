1a867d4aa415883f1fb77bf9ef2b43d46a2cc0e5
jeongye01 <74299317+jeongye01@users.noreply.github.com>
2022-04-06 11:51:31 +0900

    Update reviews.ts

--- find-my/pages/api/users/[id]/reviews.ts
+++ find-my/pages/api/users/[id]/reviews.ts
@@ -6,28 +6,61 @@ import { withApiSession } from '@libs/back/session';
 async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
   const {
     query: { id },
+    session: { user },
   } = req;
-  try {
-    const reviews = await client.review.findMany({
-      where: {
-        createdForId: +id.toString(),
-      },
-      include: { createdBy: { select: { id: true, name: true, avatar: true } } },
-    });
-    res.json({
-      ok: true,
-      reviews,
-    });
-  } catch {
-    res.json({
-      ok: false,
-    });
+  if (req.method === 'GET') {
+    try {
+      const reviews = await client.review.findMany({
+        where: {
+          createdForId: +id.toString(),
+        },
+        include: { createdBy: { select: { id: true, name: true, avatar: true } } },
+      });
+      res.json({
+        ok: true,
+        reviews,
+      });
+    } catch {
+      res.json({
+        ok: false,
+      });
+    }
+  }
+  if (req.method === 'POST') {
+    const {
+      body: { review },
+    } = req;
+    try {
+      const reviews = await client.review.create({
+        data: {
+          createdBy: {
+            connect: {
+              id: user?.id,
+            },
+          },
+          createdFor: {
+            connect: {
+              id: +id.toString(),
+            },
+          },
+          review,
+        },
+      });
+      res.json({
+        ok: true,
+        reviews,
+      });
+    } catch {
+      res.json({
+        ok: false,
+      });
+    }
   }
 }
 
 export default withApiSession(
   withHandler({
-    methods: ['GET'],
+    methods: ['GET', 'POST'],
     handler,
   }),
 );
